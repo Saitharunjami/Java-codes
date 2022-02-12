d0d6947d1f1058754570c9eadc1271962cb9366c
Jami Sai Tharun <98573953+Saitharunjami@users.noreply.github.com>
2022-02-12 18:25:39 +0530

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,11 @@
+//Given a English alphabet c, Write a program to check whether a character is a vowel or not.
+
+
+class Solution{
+        String isVowel(char c){
+    // code here
+    if (c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u' ||c == 'A' ||c == 'E' ||c == 'I' ||c == 'O' ||c == 'U'  )
+        return "YES";
+        return "NO";
+}
+    }
