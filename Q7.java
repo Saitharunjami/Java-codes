80f36b9be64d08636cb2f443a37518d326c7ebd2
Jami Sai Tharun <saitharunjami@gmail.com>
2022-06-10 11:42:26 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,6 +1,5 @@
 //Given an  unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2, …N} is missing and one number 'B' occurs twice in array. Find these two numbers. 
 
-
 class Solve {
     int[] findTwoElement(int arr[], int n) {
         // code here
