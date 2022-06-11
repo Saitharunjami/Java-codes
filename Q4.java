a7168055da610936c79f7b3fcdef36de9c85a911
Jami Sai Tharun <saitharunjami@gmail.com>
2022-06-11 23:34:10 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,6 +1,7 @@
 /*
 The first line will contain an integer T (number of test cases). First line of each test case will contain one integer c. Next line will contain a string if c is equal to 1 or will contain a integer if c is equal to 2.
 
+
 Output: 
 Print the type of the given data and the given value in new line.
 
